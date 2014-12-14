d05f5d145833976f7dcf75b9506602ab1c2c4ddf
Jacob Messina <jmessina@albany.edu>
2014-12-14 15:27:18 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,60 @@
+//mobile nav
+$('#btn-menu').click(function(){
+	$('.nav-main').slideToggle();
+});
+//panel1
+$("#panel1").click(function(){
+  $("#panelC1").toggle(1000);
+});
+//panel2
+$("#panel2").click(function(){
+  $("#panelC2").toggle(1000);
+});
+//panel3
+$("#panel3").click(function(){
+  $("#panelC3").toggle(1000);
+});
+
+//panel4
+$("#panel4").click(function(){
+  $("#panelC4").toggle(1000);
+});
+//panel5
+$("#panel5").click(function(){
+  $("#panelC5").toggle(1000);
+});
+//panel6
+$("#panel6").click(function(){
+  $("#panelC6").toggle(1000);
+});
+//panel7
+$("#panel7").click(function(){
+  $("#panelC7").toggle(1000);
+});
+//panel8
+$("#panel8").click(function(){
+  $("#panelC8").toggle(1000);
+});
+
+//panel9
+$("#panel9").click(function(){
+  $("#panelC9").toggle(1000);
+});
+
+//panel10
+$("#panel10").click(function(){
+  $("#panelC10").toggle(1000);
+});
+
+//panel8
+$("#panel11").click(function(){
+  $("#panelC11").toggle(1000);
+});
+
+
+
+$(document).ready(function(){
+    $(".nav-internal").sticky({topSpacing:5});
+  });
+  
+  
